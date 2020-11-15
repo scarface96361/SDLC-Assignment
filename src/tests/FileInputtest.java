393862bfbf8abe973bf15dbb6f6f0295f90a2cea
Joseph Rosenbaum <Joeyrosenbaum@gmail.com>
2020-11-15 16:00:45 -0500

    Added javadoc

--- src/tests/FileInputtest.java
+++ src/tests/FileInputtest.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileInputtest {
+    /** this is a file input test that mostly just tests the output of file input against itself.
+     *
+     */
     @Test
     public void testInput(){
 
